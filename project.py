e2bb7b5545ccf9704313ad326783698d035b1c78
ssi112 <brrg58@protonmail.com>
2019-08-17 11:33:56 -0400

    pre-templates

--- project.py
+++ project.py
@@ -23,7 +23,7 @@ Base.metadata.bind = engine
 # then HelloWorld() is called
 @app.route('/')
 @app.route('/restaurants/<int:restaurant_id>/')
-def restaurantMenu(restaurant_id):
+def restaurantMenu(restaurant_id = None):
     DBSession = sessionmaker(bind=engine)
     session = DBSession()
     restaurant = session.query(Restaurant).filter_by(id = restaurant_id).first()
@@ -42,6 +42,26 @@ def restaurantMenu(restaurant_id):
         output += "<html><head><title>List Menu Items</title></head><body>"
         output += "<h2>No restaurant data found for ID#{0}</h2></body>".format(restaurant_id)
         return output
+
+
+# Task 1: Create route for newMenuItem function here
+@app.route('/restaurant/<int:restaurant_id>/new')
+def newMenuItem(restaurant_id):
+    return "page to create a new menu item. Task 1 complete!"
+
+
+# Task 2: Create route for editMenuItem function here
+@app.route('/restaurant/<int:restaurant_id>/<int:menu_id>/edit')
+def editMenuItem(restaurant_id, menu_id):
+    return "page to edit a menu item. Task 2 complete!"
+
+
+# Task 3: Create a route for deleteMenuItem function here
+@app.route('/restaurant/<int:restaurant_id>/<int:menu_id>/delete')
+def deleteMenuItem(restaurant_id, menu_id):
+    return "page to delete a menu item. Task 3 complete!"
+
+
 if __name__ == '__main__':
     app.debug = True
     app.run(host='0.0.0.0', port=5000)
