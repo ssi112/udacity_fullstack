af88eb9049e22ddeae31586de2d50370ada706c8
ssi112 <brrg58@protonmail.com>
2019-08-22 12:06:28 -0400

    added message flashing

--- project.py
+++ project.py
@@ -1,6 +1,6 @@
 # minimal Flask application to get going
 #
-from flask import Flask, render_template, request, redirect, url_for
+from flask import Flask, render_template, request, redirect, url_for, flash
 app = Flask(__name__)
 
 # from lesson 1 database_setup.py
@@ -59,6 +59,7 @@ def newMenuItem(restaurant_id):
                             price = request.form['price'] )
         session.add(newItem)
         session.commit()
+        flash("New menu item created!")
         return redirect(url_for('restaurantMenu', restaurant_id=restaurant_id))
     else:
         return render_template('newmenuitem.html', restaurant_id=restaurant_id)
@@ -95,12 +96,14 @@ def deleteMenuItem(restaurant_id, menu_id):
     if request.method == 'POST':
         session.delete(itemToDelete)
         session.commit()
+        flash("Menu item deleted!")
         return redirect(url_for('restaurantMenu', restaurant_id = restaurant_id))
     return render_template('deletemenuitem.html',
             restaurant_id = restaurant_id, item = itemToDelete)
 
 
 if __name__ == '__main__':
+    app.secret_key = 'super_secret_key'
     app.debug = True
     app.run(host='0.0.0.0', port=5000)
 
